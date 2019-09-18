8f2fd2be06610191c8385790e2a1f8008832b8bb
Kuo Shao Heng <b00504059@ntu.edu.tw>
2019-09-18 19:48:41 +0900

    support for vernex

--- FR_lpdetection.py
+++ FR_lpdetection.py
@@ -1,27 +1,24 @@
-from lpdetect_model import model_WPOD
-from label_processing import predicted_label_to_origin_image
-from img_utility import cut_by_fourpts, read_img_from_dir
-from os.path import join, basename, isdir, splitext
+from label_processing import predicted_label_to_origin_image_WPOD, predicted_label_to_origin_image_Vernex_lp
+from img_utility import read_img_from_dir
+from os.path import join, basename, isdir
 from os import mkdir
-from src_others.hourglass import create_hourglass_network, bottleneck_block
 from drawing_utility import draw_LP_by_vertices
+from config import Configs
+from model_define import model_and_loss
 import numpy as np
 import cv2
 
 
-def find_outmap_high_prob(label):
-    label_batch_free = label[0]
-    img_w = 1. * label.shape[2]
-    img_h = 1. * label.shape[1]
-    for i, y in enumerate(label_batch_free):
-        for j, x in enumerate(y):
-            if x[0] > 0.9:
-                print 'high possibility =%.10f (x, y) = %.2f %.2f' % (x[0], j / img_w, i / img_h)
-
-
 # for testing SINGLE image
 # return a list of possible license plates, in each label -> [prob, cor_after_affine]
-def single_img_predict(img, input_dim=(208, 208), input_norm=True):
+def single_img_predict(img, input_dim=(0, 0), input_norm=True, model_code=''):
+    if model_code in ['WPOD+WPOD', 'Hourglass+WPOD']:
+        label_to_origin = predicted_label_to_origin_image_WPOD
+    elif model_code == 'Hourglass+Vernex_lp':
+        label_to_origin = predicted_label_to_origin_image_Vernex_lp
+    elif model_code == 'Hourglass+Vernex_lpfr':
+        pass
+
     img_shape = img.shape
     if input_norm:
         div = 255.
@@ -30,50 +27,39 @@ def single_img_predict(img, input_dim=(208, 208), input_norm=True):
     img_feed = cv2.resize(img, input_dim) / div
     img_feed = np.expand_dims(img_feed, 0)
     output_labels = model.predict(img_feed)
-    final_labels = predicted_label_to_origin_image(img_shape, output_labels[0], 16,
-                                                   prob_threshold=0.9, use_nms=True, side=3.5)
+    final_labels = label_to_origin(img_shape, output_labels[0], stride=c.stride,
+                                   prob_threshold=c.prob_threshold, use_nms=True, side=c.side)
 
     return final_labels
 
 
 if __name__ == '__main__':
 
-    '''WPOD'''
-    model = model_WPOD()
-    # this weight doesn't use norm, stride=16, side=3.5
-    model.load_weights('/home/shaoheng/Documents/Thesis_KSH/Link to training_result/CCPD_FR_2333/Dim208It79000Bsize64.h5')
+    c = Configs()
 
+    model = model_and_loss()[0]
 
-    '''Hourglass
-    model = create_hourglass_network(None, num_stacks=2, num_channels=256, inres=(256, 256), outres=(64, 64),
-                                     bottleneck=bottleneck_block)
-    # this weight uses norm, stride=4, side=16.
-    model.load_weights('/home/shaoheng/Documents/Thesis_KSH/Link to training_result/CCPD_FR_2333_hourglass/Dim256It123000Bsize16Lr0.00025.h5')
-    '''
+    model.load_weights(c.weight)
 
-    input_dir ='/home/shaoheng/Documents/Thesis_KSH/training_data/CCPD_FR_total746'
-    output_dir = '/home/shaoheng/Documents/Thesis_KSH/output_results/weight_CCPD_FR_2333_Dim208It79000Bsize64/CCPD_FR_total746'
-    if not isdir(output_dir):
-        mkdir(output_dir)
-    imgs_paths = read_img_from_dir(input_dir)
+    if not isdir(c.output_dir):
+        mkdir(c.output_dir)
+    imgs_paths = read_img_from_dir(c.input_dir)
 
     for img_path in imgs_paths:
 
         print 'processing', img_path
-        final_labels = single_img_predict(cv2.imread(img_path), input_dim=(256, 256), input_norm=False)
-        print '%d LPs found' % len(final_labels)
+        final_labels = single_img_predict(cv2.imread(img_path), input_dim=c.test_input_dim,
+                                          input_norm=c.input_norm, model_code=c.model_code)
 
         if len(final_labels) == 0:
-            print img_path, 'fail to detect'
+            print 'fail to detect'
             continue
+
+        print '%d LPs found' % len(final_labels)
 
         img = cv2.imread(img_path)
 
-        for i, final_label in enumerate(final_labels):
-            '''
-            if i != 0:
-                continue
-            '''
+        for i, final_label in enumerate(final_labels[:c.LPs_to_find]):
             prob, vertices = final_label
             try:
                 img = draw_LP_by_vertices(img, vertices)
@@ -81,7 +67,8 @@ if __name__ == '__main__':
                 print '%d LP area cutting failed' % i
                 continue
 
-        cv2.imwrite(join(output_dir, splitext(basename(img_path))[0]) + '_%d.jpg' % i, img)
+        cv2.imwrite(join(c.output_dir, basename(img_path)), img)
+        print 'write', join(c.output_dir, basename(img_path))
 
 
 
--- training.py
+++ training.py
@@ -2,83 +2,63 @@
 main training script
 remember to set 'iteration_to_load' variable to 0 when train from scratch
 """
-import lpdetect_model
-from loss import loss
-from keras.optimizers import Adam
 from label_processing import DataProvider
-from src_others.hourglass import create_hourglass_network, bottleneck_block
+from model_define import model_and_loss
 from os.path import join
 from time import time
+from config import Configs
+from keras.optimizers import Adam
+
 
 if __name__ == '__main__':
 
-    '''Config'''
-    training_dim = 256
-    stride = 4
-    out_dim = training_dim / stride
-    iterations = 300000
-    batch_size = 16
-    lr = 0.00025
-    optimizer = Adam(lr=lr)
-    record_interval = 1000
-    splice_train = False
-    CCPD_origin = False
-    side = 16.
+    c = Configs()
 
-    '''WPOD
-    model = lpdetect_model.model_WPOD()
-    '''
+    model, loss = model_and_loss()
 
-    '''Hourglass'''
-    model = create_hourglass_network(None, num_stacks=2, num_channels=256, inres=(training_dim, training_dim),
-                                     outres=(out_dim, out_dim), bottleneck=bottleneck_block)
-
-    if splice_train:
-        real_training_dim = training_dim * 2
+    if c.splice_train:
+        real_training_dim = c.training_dim * 2
     else:
-        real_training_dim = training_dim
+        real_training_dim = c.training_dim
 
-    training_data_folder = '/home/shaoheng/Documents/Thesis_KSH/training_data/CCPD_FR_total2333'
-    saving_folder = '/home/shaoheng/Documents/Thesis_KSH/Link to training_result/CCPD_FR_2333_AUG_hourglass'
+    if c.load_weight:
+        model.load_weights(join(c.saving_folder, 'Dim%dIt%dBsize%dLr%.5f.h5' %
+                                                 (real_training_dim, c.iteration_to_load, c.batch_size, c.lr)))
 
-    iteration_to_load = 0
-    # comment this line if train from scratch
-    # model.load_weights(join(saving_folder, 'Dim%dIt%dBsize%dLr%.5f.h5' %
-    #                                        (real_training_dim, iteration_to_load, batch_size, lr)))
+    elif c.transfer_learning:
+        model.load_weights(c.transfer_weight)
 
-    # if use transfer learning
-    model.load_weights('/home/shaoheng/Documents/Thesis_KSH/Link to training_result/CCPD_FR_2333_AUG_hourglass/ini_Dim256It123000Bsize16Lr0.00025.h5')
+    model.compile(loss=loss, optimizer=Adam(lr=c.lr))
 
-    model.compile(loss=loss, optimizer=optimizer)
-
-    data_provider = DataProvider(training_data_folder, batch_size, training_dim, stride=stride,
-                                 CCPD_origin=CCPD_origin, splice_train=splice_train, side=side)
+    data_provider = DataProvider(c.training_data_folder, c.batch_size, c.training_dim, stride=c.stride,
+                                 CCPD_origin=c.CCPD_origin, splice_train=c.splice_train, side=c.side,
+                                 mixing_train=c.mixing_train, model_code=c.model_code)
     data_provider.start_loading()
 
     start_time = time()
 
-    for iteration in range(iterations):
+    for iteration in range(c.iterations):
         train_x, train_y = data_provider.get_batch()
 
         print 'start training on mini batch'
         training_loss = model.train_on_batch(train_x, train_y)
 
-        total_iteration = iteration_to_load + (iteration + 1)
+        total_iteration = c.iteration_to_load + (iteration + 1)
         print 'iteration: %d, training loss:' % total_iteration, training_loss
 
         # save model
-        if (iteration + 1) % record_interval == 0:
+        if (iteration + 1) % c.record_interval == 0:
 
             time_used = time() - start_time
             start_time = time()
 
-            print 'save weight to', saving_folder
-            model.save_weights(join(saving_folder, 'Dim%dIt%dBsize%dLr%.5f.h5' %
-                                    (real_training_dim, total_iteration, batch_size, lr)))
+            print 'save weight to', c.saving_folder
+            model.save(join(c.saving_folder, 'Dim%dIt%dBsize%dLr%.5f.h5' %
+                            (real_training_dim, total_iteration, c.batch_size, c.lr)))
 
-            with open(join(saving_folder, 'record.txt'), 'a+') as f:
+            with open(join(c.saving_folder, 'record.txt'), 'a+') as f:
                 f.write('iteration %d to %d, time spent: %.2f sec, loss: %.2f\n' %
-                        (total_iteration - record_interval, total_iteration, time_used, training_loss))
+                        (total_iteration - c.record_interval, total_iteration, time_used, training_loss))
 
     data_provider.stop_loading()
 
