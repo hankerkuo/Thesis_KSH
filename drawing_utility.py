654fccb3d653d569bac76c9f3a175020a1a8f9de
Kuo Shao Heng <b00504059@ntu.edu.tw>
2019-09-18 19:46:59 +0900

    model script gathered up

--- drawing_utility.py
+++ drawing_utility.py
@@ -4,7 +4,7 @@ from img_utility import read_img_from_dir
 from CCPD_utility import FR_vertices_info, BBCor_info, vertices_info
 
 
-# img -> cv2.imread format, pts -> np.array([[x1, y1], [x2, y2], [x2, y2], [x2, y2])
+# img -> cv2.imread format, pts -> np.array([[x1, y1], [x2, y2], [x2, y2], [x2, y2]])
 def draw_LP_by_vertices(img, pts):
     cv2.polylines(img, [pts], isClosed=True, color=(255, 0, 234), thickness=4)
     return img
