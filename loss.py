4e58b1301f0596fb352289a21b76f1c7259581f3
Kuo Shao Heng <b00504059@ntu.edu.tw>
2019-09-26 20:12:05 +0900

    loss function design

--- loss.py
+++ loss.py
@@ -2,35 +2,55 @@ import tensorflow as tf
 
 
 # focal loss for localization
-def focal_loss(ptrue, ppred, szs, eps=10e-10, alpha=0.25, gama=2.0):
+def focal_loss(ptrue, ppred, szs, eps=10e-10, alpha=0.5, gama=2.0):
     b, h, w, ch = szs
-    ppred = ppred * ptrue + (1 - ptrue) - (1 - ptrue) * ppred
-    ppred = tf.clip_by_value(ppred, eps, 1.)
-    ppred = -alpha * tf.pow(1 - ppred, gama) * tf.log(ppred)
+    ppred = - tf.log(tf.clip_by_value(ppred, eps, 1.)) * ptrue * alpha * tf.pow(1 - ppred, gama) \
+            - tf.log(tf.clip_by_value(1 - ppred, eps, 1.)) * (1 - ptrue) * (1 - alpha) * tf.pow(ppred, gama)
     ppred = tf.reshape(ppred, (b, h * w * ch))
     ppred = tf.reduce_sum(ppred, axis=1)
     return ppred
 
 
-def entropy_loss(ptrue, ppred, szs, eps=10e-10):
+# focal loss for fr-auxiliary helping
+def focal_loss_fr_aux(ptrue_lp, ppred, szs, ptrue_fr, eps=10e-10, alpha=0.75, beta=0.5, gama=2.0):
     b, h, w, ch = szs
-    ppred = ppred * ptrue + (1 - ptrue) - (1 - ptrue) * ppred
+    ppred = - tf.log(tf.clip_by_value(ppred, eps, 1.)) * ptrue_lp * alpha * tf.pow(1 - ppred, gama) \
+            - tf.log(tf.clip_by_value(1 - ppred, eps, 1.)) * (1 - ptrue_lp) * (1 - alpha) * tf.pow(ppred, gama)\
+            - tf.log(tf.clip_by_value(1 - ppred, eps, 1.)) * (1 - ptrue_fr) * beta * tf.pow(ppred, gama)
+    ppred = tf.reshape(ppred, (b, h * w * ch))
+    ppred = tf.reduce_sum(ppred, axis=1)
+    return ppred
+
+
+# classic entropy loss, for binary
+def binary_entropy_loss(ptrue, ppred, szs, eps=10e-10):
+    b, h, w, ch = szs
+    ppred = - tf.log(tf.clip_by_value(ppred, eps, 1.)) * ptrue \
+            - tf.log(tf.clip_by_value(1 - ppred, eps, 1.)) * (1 - ptrue)
+    ppred = tf.reshape(ppred, (b, h * w * ch))
+    ppred = tf.reduce_sum(ppred, axis=1)
+    return ppred
+
+
+# classic entropy loss, for multi-class with softmax activation, focal version
+def focal_multi_class_entropy_loss(ptrue, ppred, szs, eps=10e-10, alpha=0.5, gama=2.0):
+    b, h, w, ch = szs
+    ppred = tf.clip_by_value(ppred, eps, 1.)
+    ppred = -tf.log(ppred) * ptrue * tf.pow(1 - ppred, gama) * alpha
+    ppred = tf.reshape(ppred, (b, h * w * ch))
+    ppred = tf.reduce_sum(ppred, axis=1)
+    return ppred
+
+
+# classic entropy loss, for multi-class with softmax activation
+def multi_class_entropy_loss(ptrue, ppred, szs, eps=10e-10):
+    b, h, w, ch = szs
     ppred = tf.clip_by_value(ppred, eps, 1.)
     ppred = -tf.log(ppred)
+    ppred = ppred * ptrue
     ppred = tf.reshape(ppred, (b, h * w * ch))
     ppred = tf.reduce_sum(ppred, axis=1)
     return ppred
-
-
-# the probability of having/not having an object at (m, n)
-def logloss(Ptrue, Pred, szs, eps=10e-10):
-    b, h, w, ch = szs
-    Pred = tf.clip_by_value(Pred, eps, 1.)
-    Pred = -tf.log(Pred)
-    Pred = Pred*Ptrue
-    Pred = tf.reshape(Pred, (b, h*w*ch))
-    Pred = tf.reduce_sum(Pred, 1)
-    return Pred
 
 
 # the error between a warped version of the canonical square
@@ -43,9 +63,11 @@ def l1(true, pred, szs):
 
 
 # Ytrue shape -> [b, h, w, 18], in '18' -> [prob_lp, prob_fr, (rxi, ryi) for i in range(4)
-#                                                             (rxj, ryj) for j in range(4)]
-# Ypred shape -> [b, h, w, 17], in '17' -> [prob_lp, prob_fr, (rxi, ryi) for i in range(4)
-#                                                             (rxj, ryj) for j in range(4)]
+#                                                             (rxj, ryj) for j in range(4)
+#                                                             class_bg, class_front, class_rear]
+# Ypred shape -> [b, h, w, 17], in '17' -> [prob_lpfr, (rxi, ryi) for i in range(4)
+#                                                      (rxj, ryj) for j in range(4)
+#                                                      class_bg, class_front, class_rear]
 def loss_Vernex_lpfr(Ytrue, Ypred):
     # b -> batch size
     b = tf.shape(Ytrue)[0]
@@ -54,14 +76,17 @@ def loss_Vernex_lpfr(Ytrue, Ypred):
 
     prob_lp_true = Ytrue[..., 0]  # [b, h, w], the dimension has been shrank
     prob_fr_true = Ytrue[..., 1]  # [b, h, w]
-    prob_lp_pred = Ypred[..., 0]  # [b, h, w]
-    prob_fr_pred = Ypred[..., 1]  # [b, h, w]
-
-    ratio_lp_pred = Ypred[..., 2:10]  # [b, h, w, 8], ratio for lp vertex expanding
-    ratio_fr_pred = Ypred[..., 10:18]  # [b, h, w, 8], ratio for fr vertex expanding
+    prob_lpfr_pred = Ypred[..., 0]
 
     pts_lp_true = Ytrue[..., 2:10]  # [b, h, w, 8]
     pts_fr_true = Ytrue[..., 10:18]
+
+    ratio_lp_pred = Ypred[..., 1:9]  # [b, h, w, 8], ratio for lp vertex expanding
+    ratio_fr_pred = Ypred[..., 9:17]  # [b, h, w, 8], ratio for fr vertex expanding
+
+    fr_class_onehot = Ytrue[..., 18:21]  # [b, h, w, 3]
+    fr_class_pred = Ypred[..., 17:20]
+    fr_region = Ytrue[..., 19] + Ytrue[..., 20]  # used for lp appears in a non-fr region penetration
 
     base = tf.stack([[[[1., 1., -1., 1., -1., -1., 1., -1.]]]])  # shape=[1, 1, 1, 8]
     base = tf.tile(base,
@@ -75,7 +100,7 @@ def loss_Vernex_lpfr(Ytrue, Ypred):
         ratio_lp = ratio_lp_pred[..., i:i + 2]  # shape = [b, h, w ,2]
         ratio_fr = ratio_fr_pred[..., i:i + 2]  # shape = [b, h, w ,2]
         pts_lp_xy = row * ratio_lp  # shape = [b, h, w, 2]
-        pts_fr_xy = row * ratio_fr
+        pts_fr_xy = row * ratio_fr  # maximum function for not changing the basic direction
 
         pts_lp_pred = tf.concat([pts_lp_pred, pts_lp_xy],
                                 3)  # final shape = [b, h, w, 8] after 4 times of concat (+2 for each loop)
@@ -86,14 +111,16 @@ def loss_Vernex_lpfr(Ytrue, Ypred):
     flags_fr = tf.reshape(prob_fr_true, (b, h, w, 1))
 
     # focal loss
-    focal_lp = 1. * focal_loss(prob_lp_true, prob_lp_pred, (b, h, w, 1))
-    focal_fr = 1. * focal_loss(prob_fr_true, prob_fr_pred, (b, h, w, 1))
+    focal_lpfr = 1. * focal_loss_fr_aux(prob_lp_true, prob_lpfr_pred, (b, h, w, 1), fr_region)
 
     # localization loss
-    loc_lp = 1. * l1(pts_lp_true * flags_lp / 64., pts_lp_pred * flags_lp / 64., (b, h, w, 4 * 2))
-    loc_fr = 1. * l1(pts_fr_true * flags_fr / 64., pts_fr_pred * flags_fr / 64., (b, h, w, 4 * 2))
+    loc_lp = 1. * l1(pts_lp_true * flags_lp, pts_lp_pred * flags_lp, (b, h, w, 4 * 2))
+    loc_fr = 1. * l1(pts_fr_true * flags_fr / 32., pts_fr_pred * flags_fr / 32., (b, h, w, 4 * 2))
 
-    res = focal_lp + focal_fr + loc_lp + loc_fr
+    # class loss
+    class_fr = 1. * focal_multi_class_entropy_loss(fr_class_onehot, fr_class_pred, (b, h, w, 3))
+
+    res = focal_lpfr + loc_lp + loc_fr + class_fr
 
     return res
 
@@ -136,8 +163,8 @@ def loss_WPOD(Ytrue, Ypred):
 
     flags = tf.reshape(obj_probs_true, (b, h, w, 1))
     res = 1.*l1(pts_true * flags, pts * flags, (b, h, w, 4 * 2))
-    res += 1.*logloss(obj_probs_true, obj_probs_pred, (b, h, w, 1))
-    res += 1.*logloss(non_obj_probs_true, non_obj_probs_pred, (b, h, w, 1))
+    res += 1.*multi_class_entropy_loss(obj_probs_true, obj_probs_pred, (b, h, w, 1))
+    res += 1.*multi_class_entropy_loss(non_obj_probs_true, non_obj_probs_pred, (b, h, w, 1))
 
     return res
 
@@ -174,6 +201,6 @@ def loss_Vernex_lp(Ytrue, Ypred):
     flags = tf.reshape(obj_probs_true, (b, h, w, 1))
     res = 1.*l1(pts_true * flags / 64., pts * flags / 64., (b, h, w, 4 * 2))
     res += 1.*focal_loss(obj_probs_true, obj_probs_pred, (b, h, w, 1))
-    # res += 1.*logloss(non_obj_probs_true, non_obj_probs_pred, (b, h, w, 1))
+    # res += 1.*multi_class_entropy_loss(non_obj_probs_true, non_obj_probs_pred, (b, h, w, 1))
 
     return res
