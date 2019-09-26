b3df2f218f42bb3c8cba6f1f29a6fbe06f8b44ab
Kuo Shao Heng <b00504059@ntu.edu.tw>
2019-09-26 20:06:59 +0900

    now can extract the license plate region and reconstruct them

--- drawing_utility.py
+++ drawing_utility.py
@@ -1,9 +1,11 @@
 import cv2
+import numpy as np
+from img_utility import pts_to_BBCor
 
 
-# img -> cv2.imread format, pts -> np.array([[x1, y1], [x2, y2], [x2, y2], [x2, y2]])
-def draw_LP_by_vertices(img, pts):
-    cv2.polylines(img, [pts], isClosed=True, color=(255, 0, 234), thickness=4)
+# img -> cv2.imread format, pts -> [[x1, y1], [x2, y2], [x2, y2], [x2, y2]]
+def draw_LP_by_vertices(img, pts, color=(255, 0, 234)):
+    cv2.polylines(img, [np.array(pts)], isClosed=True, color=color, thickness=4)
     return img
 
 
@@ -11,6 +13,39 @@ def draw_LP_by_vertices(img, pts):
 # cv2.rectangle need tuple format for points
 def draw_LP_by_BBCor(img, BBCor):
     cv2.rectangle(img, tuple(BBCor[0]), tuple(BBCor[1]), color=(255, 0, 234), thickness=4)
+    return img
+
+
+# img -> cv2.imread format, pts -> np.array([[x1, y1], [x2, y2], [x2, y2], [x2, y2]])
+# fr_class: 0 -> bg, 1 -> front, 2 -> rear
+def draw_FR_color_by_class(img, lp_prob, pts, fr_class, class_prob):
+
+    tl, br = pts_to_BBCor(*pts)
+    bl = [tl[0], br[1]]
+
+    # back ground class
+    if fr_class in [0]:
+        color = (0, 0, 0)  # BLACK
+        draw_LP_by_vertices(img, pts, color=color)
+        cv2.putText(img, 'Unknown:' + '%.3f' % class_prob, tuple(tl),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=color, thickness=3)
+    # front class
+    elif fr_class in [1]:
+        color = (36, 247, 255)  # YELLOW
+        draw_LP_by_vertices(img, pts, color=color)
+        cv2.putText(img, 'Front:' + '%.3f' % class_prob, tuple(tl),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=color, thickness=3)
+    # rear class
+    elif fr_class in [2]:
+        color = (255, 194, 36)  # LIGHT BLUE
+        draw_LP_by_vertices(img, pts, color=color)
+        cv2.putText(img, 'Rear:' + '%.3f' % class_prob, tuple(tl),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=color, thickness=3)
+
+    # lp probability
+    cv2.putText(img, 'lp_prob:' + '%.3f' % lp_prob, tuple(bl),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=(0, 0, 0), thickness=2)
+
     return img
 
 
--- geometry_calc.py
+++ geometry_calc.py
@@ -1,7 +1,9 @@
 import numpy as np
+import cv2
 from shutil import copy
 from img_utility import read_img_from_dir
 from dataset_utility import CCPD_vertices_info
+from shapely.geometry import Polygon, Point
 
 # return the horizontal and vertical rotation degree of a LP by giving its four vertices
 # vertices format -> [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] from br and clockwise
@@ -36,7 +38,39 @@ def pick_range_of_angle(img_folder, output_folder, hor_degree_threshold, ver_deg
             copy(img_path, output_folder)
 
 
+# perspective warp: from original predicted license plate vertices to a planar rectangle
+# width and height need to be defined
+def planar_rectification(img, vertices, width=400, height=100):
+    vertices = np.array([tuple(vertice) for vertice in vertices]).astype(np.float32)  # vertices is from br and clockwise
+    dst = np.array([(width, height), (0, height), (0, 0), (width, 0)]).astype(np.float32)
+    maxtrix = cv2.getPerspectiveTransform(vertices, dst)
+    lp_after_transform = cv2.warpPerspective(img, M=maxtrix, dsize=(width, height))
+
+    return lp_after_transform
+
+
+# the polygon iou of between a polygon and the same polygon on a specific point
+def polygon_iou(polygon_vertices, pt):
+    polygon_vertices = np.array(polygon_vertices)
+    poly_1 = Polygon([tuple(vertice) for vertice in polygon_vertices])
+    centroid = np.array(poly_1.centroid)
+
+    poly_on_pt = polygon_vertices + np.array(pt) - centroid
+    poly_2 = Polygon([tuple(vertice) for vertice in poly_on_pt])
+
+    return poly_1.intersection(poly_2).area / poly_1.union(poly_2).area
+
+
+# return True if the point is within the polygon, else return False
+def pt_within_polygon(pt, polygon_vertices):
+    polygon_vertices = np.array(polygon_vertices)
+    polygon = Polygon([tuple(vertice) for vertice in polygon_vertices])
+    point = Point(tuple(pt))
+
+    return point.within(polygon)
+
+
 if __name__ == '__main__':
-    img_folder = '/home/shaoheng/Documents/cars_label_FRNet/CCPD_2019_first_part/ccpd_base'
-    output_folder = '/home/shaoheng/Documents/cars_label_FRNet/CCPD_2019_first_part/rotated_and_tilted'
-    pick_range_of_angle(img_folder, output_folder, hor_degree_threshold=20, ver_degree_threshold=10)
+    pt = (50, 50)
+    polygon = Polygon([(95, 100), (10, 50), (5, 10), (80, 60)])
+    print pt.within(polygon)
