4cb3c0119523e8e1a9a99ddf4841631c7511b196
Kuo Shao Heng <b00504059@ntu.edu.tw>
2019-09-26 20:08:54 +0900

    data aug and label encoding strategy changed

--- data_aug.py
+++ data_aug.py
@@ -1,36 +1,38 @@
 import numpy as np
 import imgaug.augmenters as iaa
 from img_utility import read_img_from_dir
-from dataset_utility import CCPD_FR_vertices_info, CCPD_vertices_info, CCPD_FR_front_rear_info
+from dataset_utility import CCPD_FR_vertices_info, CCPD_FR_front_rear_info
+from dataset_utility import vernex_vertices_info, vernex_front_rear_info, vernex_fr_class_info
 from drawing_utility import draw_LP_by_vertices
 import cv2
 from config import Configs
 
 
-# now CCPD_origin=True is not support for the front-rear BB augmentation
-def data_aug(img_paths, CCPD_origin=False):
+def data_aug(img_paths):
     c = Configs()
 
     sometimes = lambda aug: iaa.Sometimes(0.5, aug)
     imgs = []
     key_pts = []
+    fr_classes = []
 
     for img_path in img_paths:
         img = cv2.imread(img_path)
         cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         imgs.append(img)
 
-        if CCPD_origin:
-            if c.model_code in ['Hourglass+Vernex_lp', 'Hourglass+WPOD', 'WPOD+WPOD']:
-                vertices = np.array(CCPD_vertices_info(img_path))
-            elif c.model_code == 'Hourglass+Vernex_lpfr':
-                print 'function not implemented, pass'
-                pass
-        elif not CCPD_origin:
-            if c.model_code in ['Hourglass+Vernex_lp', 'Hourglass+WPOD', 'WPOD+WPOD']:
-                vertices = np.array(CCPD_FR_vertices_info(img_path))
-            elif c.model_code == 'Hourglass+Vernex_lpfr':
-                vertices = np.array(CCPD_FR_vertices_info(img_path) + CCPD_FR_front_rear_info(img_path))
+        if c.dataset_code in ['CCPD_FR']:
+            vertices_info = CCPD_FR_vertices_info
+            front_rear_info = CCPD_FR_front_rear_info
+        elif c.dataset_code in ['vernex']:
+            vertices_info = vernex_vertices_info
+            front_rear_info = vernex_front_rear_info
+            fr_classes.append(vernex_fr_class_info(img_path))
+
+        if c.model_code in ['Hourglass+Vernex_lp', 'Hourglass+WPOD', 'WPOD+WPOD']:
+            vertices = np.array(vertices_info(img_path))
+        elif c.model_code == 'Hourglass+Vernex_lpfr':
+            vertices = np.array(vertices_info(img_path) + front_rear_info(img_path))
 
         key_pts.append(vertices)
 
@@ -50,17 +52,17 @@ def data_aug(img_paths, CCPD_origin=False):
     for img_aug in imgs_aug:
         cv2.cvtColor(img_aug, cv2.COLOR_RGB2BGR)
 
-    return imgs_aug, vertices_aug
+    return imgs_aug, vertices_aug, fr_classes
 
 
 if __name__ == '__main__':
-    img_paths = read_img_from_dir('/home/shaoheng/Documents/Thesis_KSH/training_data/openALPR_us')[:32]
+    img_paths = read_img_from_dir('/home/shaoheng/Documents/Thesis_KSH/training_data/kr_tilt_vernex')[:]
     while 1:
-        images_aug, keypoints_aug = data_aug(img_paths)
-        for image_aug, keypoint_aug in zip(images_aug, keypoints_aug):
+        images_aug, keypoints_aug, fr_classes = data_aug(img_paths)
+        for image_aug, keypoint_aug, fr_class in zip(images_aug, keypoints_aug, fr_classes):
             image_aug = draw_LP_by_vertices(image_aug, keypoint_aug[0:4])
             image_aug = draw_LP_by_vertices(image_aug, keypoint_aug[4:8])
-            cv2.imwrite('0.jpg', image_aug)
+            print fr_class
             cv2.imshow('img', image_aug)
             cv2.waitKey(0)
 
--- label_processing.py
+++ label_processing.py
@@ -3,13 +3,12 @@ this script is used to process the CCPD_FR labels and make them fit the required
 """
 import cv2
 import numpy as np
+
 from img_utility import pts_to_BBCor, read_img_from_dir, pixel_to_ratio, IoU
-from dataset_utility import CCPD_FR_vertices_info, CCPD_vertices_info
-from random import sample, shuffle
+from dataset_utility import CCPD_FR_vertices_info
 from collections import deque
-from threading import Thread, Lock
-from time import sleep
 from data_aug import data_aug
+from geometry_calc import polygon_iou, pt_within_polygon
 
 
 """
@@ -39,7 +38,7 @@ def mean_size_LP(img_folder, training_dim, total_stride=1):
 # need to give the dimension for training and the total stride in the model
 # label shape -> [y, x, 1 + 2*4], y and x are the downsampled output map size
 # label format -> [object_1or0, x1, y1, x2, y2, x3, y3, x4, y4] pts from bottom right and clockwise
-def CCDP_FR_to_training_label(img_path, training_dim, stride, CCPD_origin=False, side=3.5):
+def CCDP_FR_to_training_label(img_path, training_dim, stride, side=3.5):
 
     # side = 3.5 calculated by training_dim = 208 and stride = 16 in 746 dataset
     # side = 16. calculated by training_dim = 256 and stride = 4 in 2333 dataset
@@ -48,10 +47,7 @@ def CCDP_FR_to_training_label(img_path, training_dim, stride, CCPD_origin=False,
 
     assert training_dim % stride == 0, 'training_dim dividing stride must be a integer'
 
-    if CCPD_origin:
-        vertices = CCPD_vertices_info(img_path)
-    else:
-        vertices = CCPD_FR_vertices_info(img_path)
+    vertices = CCPD_FR_vertices_info(img_path)
 
     LP_Cor = np.array(pixel_to_ratio(img_shape, *vertices)) * training_dim
     LP_BB = np.array(pts_to_BBCor(*LP_Cor))
@@ -81,11 +77,11 @@ def CCDP_FR_to_training_label(img_path, training_dim, stride, CCPD_origin=False,
 
 # batch version of label conversion, with data augmentation!
 # WPOD-based encoding
-def batch_CCPD_to_training_label(img_paths, training_dim, stride, CCPD_origin=False, side=3.5):
+def batch_CCPD_to_training_label(img_paths, training_dim, stride, side=3.5):
 
     x_labels = []
     y_labels = []
-    imgs_aug, vertices_aug = data_aug(img_paths, CCPD_origin=CCPD_origin)
+    imgs_aug, vertices_aug = data_aug(img_paths)
 
     for img_aug, vertice_aug in zip(imgs_aug, vertices_aug):
         # side = 3.5 calculated by training_dim = 208 and stride = 16 in 746 dataset
@@ -126,16 +122,17 @@ def batch_CCPD_to_training_label(img_paths, training_dim, stride, CCPD_origin=Fa
 
 # batch version of label conversion, with data augmentation!
 # vernex-based encoding
-# label format -> [prob_lp, prob_fr, x1, y1, x2, y2, x3, y3, x4, y4  ->> lp coordinates
-#                                    x1, y1, x2, y2, x3, y3, x4, y4] ->> fr coordinates
+# label format -> [prob_lp, prob_fr, x1, y1, x2, y2, x3, y3, x4, y4,  ->> lp coordinates
+#                                    x1, y1, x2, y2, x3, y3, x4, y4,  ->> fr coordinates
+#                                    class_bg, class_front, class_rear] ->> for classification
 #                                    pts from bottom right and clockwise
-def batch_CCPD_to_training_label_vernex_lpfr(img_paths, training_dim, stride, CCPD_origin=False, side=3.5):
+def batch_CCPD_to_training_label_vernex_lpfr(img_paths, training_dim, stride, side=3.5):
 
     x_labels = []
     y_labels = []
-    imgs_aug, vertices_aug = data_aug(img_paths, CCPD_origin=CCPD_origin)
+    imgs_aug, vertices_aug, fr_classes = data_aug(img_paths)
 
-    for img_aug, vertice_aug in zip(imgs_aug, vertices_aug):
+    for img_aug, vertice_aug, fr_class in zip(imgs_aug, vertices_aug, fr_classes):
         # side = 3.5 calculated by training_dim = 208 and stride = 16 in 746 dataset
         # side = 16. calculated by training_dim = 256 and stride = 4 in 2333 dataset
         img_shape = img_aug.shape
@@ -158,7 +155,8 @@ def batch_CCPD_to_training_label_vernex_lpfr(img_paths, training_dim, stride, CC
         LP_BB_outdim = [np.maximum(LP_BB[0] / stride, 0).astype(int), np.minimum(LP_BB[1] / stride, out_size).astype(int)]
         FR_BB_outdim = [np.maximum(FR_BB[0] / stride, 0).astype(int), np.minimum(FR_BB[1] / stride, out_size).astype(int)]
 
-        y_label = np.zeros((out_size, out_size, 2 + 2 * 4 + 2 * 4))
+        y_label = np.zeros((out_size, out_size, 2 + 2 * 4 + 2 * 4 + 3))
+        y_label[..., 18] = 1  # class_bg encoding
 
         # LP encoding
         for y in range(LP_BB_outdim[0][1], LP_BB_outdim[1][1]):
@@ -185,10 +183,20 @@ def batch_CCPD_to_training_label_vernex_lpfr(img_paths, training_dim, stride, CC
                 same_BB_on_now_pixel = [now_pixel - FR_BB_wh / 2., now_pixel + FR_BB_wh / 2.]
                 iou = IoU(FR_BB_outdim, same_BB_on_now_pixel)
 
-                if iou > 0.7:
+                if iou > 0.4:
                     FR_Cor_recenter = (np.array(FR_Cor_outdim) - now_pixel) / side
                     y_label[y, x, 1] = 1
                     y_label[y, x, 10:18] = FR_Cor_recenter.flatten()
+
+                # used for context-auxiliary training and classification, so the region needs to be accurate as possible
+                # if polygon_iou(FR_Cor_outdim, now_pixel) > 0.2:
+                if pt_within_polygon(now_pixel, FR_Cor_outdim):
+                    if fr_class in ['front']:
+                        y_label[y, x, 18] = 0
+                        y_label[y, x, 19] = 1  # front one-hot encoding
+                    elif fr_class in ['rear']:
+                        y_label[y, x, 18] = 0
+                        y_label[y, x, 20] = 1  # rear one-hot encoding
 
         x_label = cv2.resize(img_aug, (training_dim, training_dim)) / 255.  # 255 for normalization
 
@@ -220,134 +228,6 @@ def img_splicing(img1, img2, img3, img4):
     final_img = np.concatenate([top_row, bottom_row], axis=0)
 
     return final_img
-
-
-# the class provides batch training data
-# need to give arguments -> img_folder, batch_size, training_dim, stride
-# it can be served as 1. a infinite iterator which keeps providing data, each image in the folder will be provided
-#                        before next epoch, the image will be randomly selected (the 'shuffle' argument) in every epoch
-#                     2. a daemon threading data provider, which is preferred and much faster than the iterator
-# * if set splice_train = true, then the output training data's dim will be twice the given value
-class DataProvider:
-
-    def __init__(self, img_folder, batch_size, training_dim, stride, CCPD_origin=False,
-                 shuffle=True, splice_train=False, mixing_train=False, side=3.5, model_code=''):
-        self.img_folder = img_folder
-        self.batch_size = batch_size
-        self.training_dim = training_dim
-        self.stride = stride
-        self.out_dim = training_dim / stride
-        self.CCPD_origin = CCPD_origin
-        self.shuffle = shuffle
-        self.splice_train = splice_train
-        self.mixing_train = mixing_train
-        self.side = side
-        self.x_data, self.y_data = self.create_buffer(batch_size)
-        self.buffer_loaded = False
-        self._lock = Lock()
-        self.thread = Thread()
-        self.stop_buffer = False
-        self.imgs_paths = []
-        self.renew_img_paths()
-        self.samples = deque(self.imgs_paths)
-        '''MODEL, in ['Hourglass+Vernex_lpfr', 'Hourglass+Vernex_lp', 'Hourglass+WPOD', 'WPOD+WPOD']'''
-        if model_code in ['Hourglass+Vernex_lp', 'Hourglass+WPOD', 'WPOD+WPOD']:
-            self.to_training_label = batch_CCPD_to_training_label
-        elif model_code == 'Hourglass+Vernex_lpfr':
-            self.to_training_label = batch_CCPD_to_training_label_vernex_lpfr
-
-    def __iter__(self):
-        if shuffle:
-            shuffle(self.samples)
-        return self
-
-    def next(self):
-        x_data = []
-        y_data = []
-
-        for b in range(self.batch_size):
-            if len(self.samples) == 0:
-                self.samples = deque(self.imgs_paths)
-                if shuffle:
-                    shuffle(self.samples)
-                break
-            img_path = self.samples.pop()
-            x_data.append(cv2.resize(cv2.imread(img_path), (self.training_dim, self.training_dim)))
-            y_data.append(CCDP_FR_to_training_label(img_path, self.training_dim,
-                                                    self.stride, CCPD_origin=self.CCPD_origin, side=self.side))
-
-        if len(x_data) == 0:
-            return self.next()
-        else:
-            return np.array(x_data), np.array(y_data)
-
-    def create_buffer(self, batch_size):
-        x = np.empty((batch_size, self.training_dim, self.training_dim, 3))
-        y = np.empty((batch_size, self.out_dim, self.out_dim, 1 + 2 * 4))
-        return x, y
-
-    def renew_img_paths(self):
-        if self.mixing_train:
-            CCPD_FR = read_img_from_dir('/home/shaoheng/Documents/Thesis_KSH/training_data/CCPD_FR')
-            openALPR_br = read_img_from_dir('/home/shaoheng/Documents/Thesis_KSH/training_data/openALPR_br')
-            openALPR_us = read_img_from_dir('/home/shaoheng/Documents/Thesis_KSH/training_data/openALPR_us')
-            openALPR_eu = read_img_from_dir('/home/shaoheng/Documents/Thesis_KSH/training_data/openALPR_eu')
-            CCPD_FR = sample(CCPD_FR, 200)
-
-            self.imgs_paths = openALPR_br + openALPR_us + openALPR_eu + CCPD_FR
-
-        elif not self.mixing_train:
-            self.imgs_paths = read_img_from_dir(self.img_folder)
-
-    def get_batch(self):
-        while self.buffer_loaded is False:
-            sleep(0.01)
-        with self._lock:
-            self.buffer_loaded = False
-            return self.x_data, self.y_data
-
-    def load(self):
-        while True:
-            while self.buffer_loaded is True:
-                sleep(0.01)
-                if self.stop_buffer:
-                    return 0
-            with self._lock:
-                img_paths = []
-                for b in range(self.batch_size):
-                    # print 'now %d samples left' % len(self.samples)
-                    if len(self.samples) == 0:
-                        self.renew_img_paths()
-                        self.samples = deque(self.imgs_paths)
-                        if shuffle:
-                            shuffle(self.samples)
-                        break
-                    img_paths.append(self.samples.pop())
-                if len(img_paths) == 0:
-                    return self.load()
-
-                x_data, y_data = self.to_training_label(img_paths, self.training_dim, self.stride,
-                                                        CCPD_origin=self.CCPD_origin, side=self.side)
-                self.x_data = np.array(x_data)
-                self.y_data = np.array(y_data)
-                self.buffer_loaded = True
-                print 'loading batch image to buffer... done!'
-
-    def start_loading(self):
-        self.thread = Thread(target=self.load)
-        self.thread.setDaemon(True)
-        self.thread.start()
-
-    def stop_loading(self):
-        self.stop_buffer = True
-        self.thread.join()
-
-    def load_whole_folder(self):  # risky
-        images = []
-        for i, sample in enumerate(self.samples):
-            images.append(cv2.imread(sample))
-            print 1. * i / len(self.samples)
-        return images
 
 
 """
@@ -447,16 +327,74 @@ def predicted_label_to_origin_image_Vernex_lp(ori_image_shape, label, stride, pr
     return label_to_origin
 
 
-# nms function, labels -> a list of labels, its element is [probability, coordinates]
-def nms(labels, threshold=0.5):
+# receive the output of the network and map the label to the original image
+# now this function only work with single image prediction label
+# in each label -> [prob, vertex_predicted_lp, vertex_predicted_fr, [fr_class, class_prob]]
+def predicted_label_to_origin_image_Vernex_lpfr(ori_image_shape, label, stride, prob_threshold=0.9, use_nms=True, side=3.5):
+    out_w = label.shape[1]
+    out_h = label.shape[0]
+
+    label_to_origin = []
+    for y in range(out_h):
+        for x in range(out_w):
+            prob = label[y, x, 0]      # integer
+            if prob >= prob_threshold:
+                single_label = []
+                single_label.append(prob)
+                now_pixel = np.array([x + 0.5, y + 0.5])
+
+                ratio_lp = label[y, x, 1:9]
+                ratio_fr = label[y, x, 9:17]
+                ratio_lp, ratio_fr = np.reshape(ratio_lp, (4, 2)), np.reshape(ratio_fr, (4, 2))
+                # base vectors from br and clock-wise
+                base_vector = np.array([[1, 1], [-1, 1], [-1, -1], [1, -1]])
+
+                # predicted vertices -> [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+                for ratio in [ratio_lp, ratio_fr]:
+                    vertices = base_vector * ratio  # shape = [4, 2]
+                    vertices = vertices * side
+                    vertices += now_pixel
+                    vertices *= stride
+                    vertices *= np.array([ori_image_shape[1] / (1. * out_w * stride),
+                                          ori_image_shape[0] / (1. * out_h * stride)])
+                    vertices = vertices.astype(int)
+
+                    # clip according to the size of original size of image
+                    for pts in vertices:
+                        pts[0] = np.clip(pts[0], 0, ori_image_shape[1])
+                        pts[1] = np.clip(pts[1], 0, ori_image_shape[0])
+
+                    single_label.append(vertices)
+
+                fr_class_prob = label[y, x, 17:20]
+                fr_class = np.argmax(fr_class_prob)
+                single_label.append([fr_class, fr_class_prob[fr_class]])
+
+                label_to_origin.append(single_label)
+    if use_nms:
+        label_to_origin = nms(label_to_origin)
+
+    return label_to_origin
+
+
+# nms function, labels -> a list of labels, its element is [probability, vertex_predicted_lp, vertex_predicted_fr,
+#                                                           [fr_class, class_prob]]
+def nms(labels, threshold=0.1, nms_ref='fr'):
     labels.sort(key=lambda x: x[0], reverse=True)
     labels = deque(labels)
     labels_nms = []
+
+    assert nms_ref in ['lp', 'fr'], 'NMS reference must be lp or fr!'
+    if nms_ref in ['lp']:
+        index = 1
+    elif nms_ref in ['fr']:
+        index = 2
+
     while len(labels) > 0:
         now_handle = labels.popleft()
         overlap = False
         for label_nms in labels_nms:
-            if IoU(pts_to_BBCor(*now_handle[1]), pts_to_BBCor(*label_nms[1])) > threshold:
+            if IoU(pts_to_BBCor(*now_handle[index]), pts_to_BBCor(*label_nms[index])) > threshold:
                 overlap = True
                 break
         if not overlap:
@@ -464,20 +402,5 @@ def nms(labels, threshold=0.5):
     return labels_nms
 
 
-if __name__ == '__main__':
-    path = '/home/shaoheng/Documents/Thesis_KSH/training_data/CCPD_FR'
-    data_generator = DataProvider(path, 32, 256, 4, side=16., mixing_train=True)
-    data_generator.start_loading()
-    while 1:
-        x, y = data_generator.get_batch()
-        print np.shape(y)
-        # data_generator.renew_img_paths()
-        # imgs_paths = data_generator.imgs_paths
-        # shuffle(imgs_paths)
-        # for img_path in imgs_paths:
-        #     cv2.imshow(img_path, cv2.imread(img_path))
-        #     cv2.moveWindow(img_path, 0, 0)
-        #     cv2.waitKey(0)
-        #     cv2.destroyAllWindows()
 
 
