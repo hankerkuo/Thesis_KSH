f673aeae5c15f741d724455fa0b3ed9272ebb919
Kuo Shao Heng <b00504059@ntu.edu.tw>
2019-09-26 20:10:21 +0900

    multi-scale training and lp drawing for detection script

--- prediction.py
+++ prediction.py
@@ -7,13 +7,15 @@ from drawing_utility import draw_LP_by_vertices, draw_FR_color_by_class
 from geometry_calc import planar_rectification
 from config import Configs
 from model_define import model_and_loss
+from time import time
 import numpy as np
 import cv2
 
 
 # for testing SINGLE image
-# return a list of possible license plates, in each label -> [prob, cor_after_affine]
-def single_img_predict(img, input_dim=(0, 0), input_norm=True, model_code=''):
+# return a list of possible license plates, in each label -> [prob, vertex_predicted_lp,
+#                              in 'lpfr', additional info -> vertex_predicted_fr, [fr_class, class_prob]]
+def single_img_predict(img_path, input_dim=(0, 0), input_norm=True, model_code=''):
     if model_code in ['WPOD+WPOD', 'Hourglass+WPOD']:
         label_to_origin = predicted_label_to_origin_image_WPOD
     elif model_code in ['Hourglass+Vernex_lp']:
@@ -21,6 +23,7 @@ def single_img_predict(img, input_dim=(0, 0), input_norm=True, model_code=''):
     elif model_code in ['Hourglass+Vernex_lpfr']:
         label_to_origin = predicted_label_to_origin_image_Vernex_lpfr
 
+    img = cv2.imread(img_path)
     img_shape = img.shape
     if input_norm:
         div = 255.
@@ -28,11 +31,15 @@ def single_img_predict(img, input_dim=(0, 0), input_norm=True, model_code=''):
         div = 1.
     img_feed = cv2.resize(img, input_dim) / div
     img_feed = np.expand_dims(img_feed, 0)
+
+    start_pred = time()
     output_labels = model.predict(img_feed)
+    time_spent = time() - start_pred
+
     final_labels = label_to_origin(img_shape, output_labels[0], stride=c.stride,
                                    prob_threshold=c.prob_threshold, use_nms=True, side=c.side)
 
-    return final_labels
+    return final_labels, time_spent
 
 
 if __name__ == '__main__':
@@ -46,13 +53,14 @@ if __name__ == '__main__':
     if not isdir(c.output_dir):
         mkdir(c.output_dir)
     imgs_paths = read_img_from_dir(c.input_dir)
-
+    time_spent = 0
     for img_path in imgs_paths:
 
         print 'processing', img_path
-        final_labels = single_img_predict(cv2.imread(img_path), input_dim=c.test_input_dim,
-                                          input_norm=c.input_norm, model_code=c.model_code)
+        final_labels, sec = single_img_predict(img_path=img_path, input_dim=c.test_input_dim,
+                                               input_norm=c.input_norm, model_code=c.model_code)
 
+        time_spent += sec
         if len(final_labels) == 0:
             print 'fail to detect'
         else:
@@ -66,8 +74,10 @@ if __name__ == '__main__':
             vertices_lp = vertices_rearange(vertices_lp)
 
             # save each license plate
+            '''
             lp_img = planar_rectification(img, vertices_lp)
             cv2.imwrite(join(c.output_dir, splitext(basename(img_path))[0] + '_%d' % i + '.jpg'), lp_img)
+            '''
 
             # draw visualization results
             img = draw_LP_by_vertices(img, vertices_lp)
@@ -80,6 +90,7 @@ if __name__ == '__main__':
         cv2.imwrite(join(c.output_dir, basename(img_path)), img)
         print 'write to:', join(c.output_dir, basename(img_path))
 
+    print 'processing, %d images, spend:%.3f seconds' % (len(imgs_paths), time_spent)
 
 
 
--- training.py
+++ training.py
@@ -2,13 +2,12 @@
 main training script
 remember to set 'iteration_to_load' variable to 0 when train from scratch
 """
-from label_processing import DataProvider
+from data_provider import DataProvider
 from model_define import model_and_loss
 from os.path import join
 from time import time
 from config import Configs
 from keras.optimizers import Adam
-
 
 if __name__ == '__main__':
 
@@ -17,28 +16,37 @@ if __name__ == '__main__':
     model, loss = model_and_loss()
 
     if c.splice_train:
-        real_training_dim = c.training_dim * 2
+        real_training_dim = c.training_dim_1 * 2
     else:
-        real_training_dim = c.training_dim
+        real_training_dim = c.training_dim_1
 
     if c.load_weight:
         model.load_weights(join(c.saving_folder, 'Dim%dIt%dBsize%dLr%.5f.h5' %
                                                  (real_training_dim, c.iteration_to_load, c.batch_size, c.lr)))
 
     elif c.transfer_learning:
-        model.load_weights(c.transfer_weight)
+        model.load_weights(c.transfer_weight, by_name=True)
 
     model.compile(loss=loss, optimizer=Adam(lr=c.lr))
 
-    data_provider = DataProvider(c.training_data_folder, c.batch_size, c.training_dim, stride=c.stride,
-                                 CCPD_origin=c.CCPD_origin, splice_train=c.splice_train, side=c.side,
-                                 mixing_train=c.mixing_train, model_code=c.model_code)
-    data_provider.start_loading()
+    data_provider_1 = DataProvider(c.training_data_folder, c.batch_size, c.training_dim_1, stride=c.stride,
+                                   splice_train=c.splice_train, side=c.side,
+                                   mixing_train=c.mixing_train, model_code=c.model_code)
+    data_provider_1.start_loading()
+
+    # multi-scale training
+    '''
+    data_provider_2 = DataProvider(c.training_data_folder, c.batch_size / 4, c.training_dim_2, stride=c.stride,
+                                   splice_train=c.splice_train, side=c.side,
+                                   mixing_train=c.mixing_train, model_code=c.model_code)
+    data_provider_2.start_loading()
+    '''
 
     start_time = time()
 
     for iteration in range(c.iterations):
-        train_x, train_y = data_provider.get_batch()
+
+        train_x, train_y = data_provider_1.get_batch()
 
         print 'start training on mini batch'
         training_loss = model.train_on_batch(train_x, train_y)
@@ -60,6 +68,7 @@ if __name__ == '__main__':
                 f.write('iteration %d to %d, time spent: %.2f sec, loss: %.2f\n' %
                         (total_iteration - c.record_interval, total_iteration, time_used, training_loss))
 
-    data_provider.stop_loading()
+    data_provider_1.stop_loading()
+    # data_provider_2.stop_loading()
 
 
